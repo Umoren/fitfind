46fe86fedaa3e6c75eac8d1ff649cb237bf318b9
John Otu <johnne01@gmail.com>
2019-07-29 16:23:41 +0000

    initial commit
    basic fitfind page with bootstrap

---
+++ src/fitfind/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FitFind = () => {
+  return (
+    <div className="container">
+      <h1 className="display-4">FitFind</h1>
+    </div>
+  );
+}
+
+export default FitFind;
