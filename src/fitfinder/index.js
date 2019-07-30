b8c455b845858f372e1ae5c6f4c2efe799eeef7a
John Otu <johnne01@gmail.com>
2019-07-30 11:23:26 +0000

    added query section
    for location input and search results

--- src/fitfinder/index.js
+++ src/fitfinder/index.js
@@ -4,6 +4,46 @@ const FitFinder = () => {
   return (
     <div className="container">
       <h1 className="display-4">FitFinder</h1>
+      <div className="row">
+        <div className="col-sm-4">
+          <div className="">
+            <label for="search" className="">Search by location</label>
+            <div className="form-inline">
+            <input type="text" className="form-control w-50 mr-2" id="search" placeholder="Enter location" />
+  
+              <button className="btn btn-primary btn-sm">Use location</button>
+              </div>
+          </div>
+          
+          <div className="mt-4">
+            <h4>Filters</h4>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="has_gym" />
+                <label className="form-check-label" for="has_gym">
+                  Has a gym
+  </label>
+</div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="has_swimming_pool" />
+              <label className="form-check-label" for="has_swimming_pool">
+                Has a swimming pool
+  </label>
+            </div>
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="has_tennis_courts" />
+              <label className="form-check-label" for="has_tennis_courts">
+                Has tennis courts
+            </label>
+            </div>
+          </div>
+
+          <div className="mt-4">
+            <h4>Results</h4>
+            </div>
+
+        </div>
+        <div className="col-sm-8"></div>
+      </div>
     </div>
   );
 }
